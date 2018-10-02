6e9159e1457ac1741353a86f7f93cd6fe36351cf
Tracy Somervell <tsomerve@gmail.com>
2018-10-01 20:30:33 -0700

    added fonts

--- poems/static/poems/sketch.js
+++ poems/static/poems/sketch.js
@@ -8,16 +8,52 @@ let myFont
 
 const fonts = [
     '/static/AlexBrush-Regular.ttf',
-    '/static/the_breakdown.ttf'
+    '/static/the_breakdown.ttf',
+    '/static/Charmonman-Regular.ttf',
+    '/static/GreatVibes-Regular.ttf',
+    '/static/KaushanScript-Regular.ttf',
 ]
 
 function preload() {
     myFont = loadFont(random(fonts))
 }
 
+class Particles {
+    constructor (x, y) {
+        this.x = x
+        this.y = y
+        this.xVel = Math.random() 
+        this.yVel = Math.random() 
+        
+        this.angle = Math.random() * 100
+        this.time = 0
+        this.willDie = (Math.random() * 100)
+        
+        this.colors = [random(0, 255), random(0, 255), random(0, 255)]
+    }
+
+    update() {
+        this.x += this.xVel
+        this.y += this.yVel
+        
+        this.time++
+        this.angle += 0.1
+    }
+    
+    render() {
+        noStroke()
+        
+        const opacity = map(this.time, this.willDie, 150, 150, 225)
+        fill(color(this.colors[0], this.colors[1], this.colors[2], opacity))
+        ellipse(this.x, this.y, this.time, this.time)
+    }
+}
+
+const particles = []
+
 function setup() {
     background(204)
-
+    
     const poem = document.querySelector('#poem')
     if(poem) {
         currentPoem = poem.innerText
@@ -30,6 +66,23 @@ function setup() {
 
 function draw() {
     background(color(155, 75, 100))
+
+    console.log(windowWidth)
+    
+    if(frameCount % 50 == 0) {
+        for (let i = 0; i < 100; i++) {
+            particles.push(new Particles(random(0, windowWidth), random(0, windowHeight)))
+        }
+    }
+    for (let i = 0; i < particles.length; i++) {
+        if (particles[i].time < particles[i].willDie) {
+            particles[i].update()
+            particles[i].render()
+        } else {
+            particles.splice(i, 1)
+        }
+    }
+    
     textSize(40)
     textStyle(ITALIC)
     textAlign(CENTER)
@@ -37,11 +90,16 @@ function draw() {
     if(currentPoem === '') {
         fill(color(255, 255, 255, 128))
         textSize(100)
-        text('Create a poem!', windowWidth / 2, windowHeight / 4)
+        text('Is this Poetry?', windowWidth / 2, windowHeight / 4)
     } else {
         fill(245);
         text(currentPoem, windowWidth / 2, windowHeight / 2, 700, windowHeight / 2)
-    }    
+    }
+    
+    rect(windowWidth / 2, windowHeight / 2, 200, 50)
+    fill(color(255, 255, 0, 0))
+    textSize(40)
+    text('Save Poem', windowWidth / 2 - 700, windowHeight / 2 - 700)
 }
 
 function mousePressed() {
@@ -52,4 +110,5 @@ function mousePressed() {
         })
         .then(() => console.log('It worked!'))
     })
+    
 }
